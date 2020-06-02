8f5ad7ab689d4e276e965729b6175ec7b1cdd0c3
Alexander Kobelev <ak.mail.ru@gmail.com>
2020-06-02 22:16:37 +0300

    Add main FX context

--- src/main/java/com/ak/fx/desktop/FxApplication.java
+++ src/main/java/com/ak/fx/desktop/FxApplication.java
@@ -1,9 +1,14 @@
 package com.ak.fx.desktop;
 
+import java.io.IOException;
+
 import javax.annotation.Nonnull;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -11,6 +16,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class FxApplication extends Application {
+  private static final String SCENE_XML = "scene.fxml";
   private ConfigurableApplicationContext applicationContext;
 
   public static void main(@Nonnull String[] args) {
@@ -23,7 +29,12 @@ public class FxApplication extends Application {
   }
 
   @Override
-  public void start(Stage primaryStage) {
+  public void start(Stage primaryStage) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(SCENE_XML));
+    fxmlLoader.setControllerFactory(applicationContext::getBean);
+
+    Parent parent = fxmlLoader.load();
+    primaryStage.setScene(new Scene(parent, 800, 600));
     primaryStage.show();
     primaryStage.centerOnScreen();
   }
